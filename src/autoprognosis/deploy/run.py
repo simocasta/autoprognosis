1298f45194f68a2473e1e0394f25ff8e08e52969
simocasta <simocasta92@gmail.com>
2023-04-17 22:26:50 +0100

    Update run.py

--- src/autoprognosis/deploy/run.py
+++ src/autoprognosis/deploy/run.py
@@ -26,7 +26,6 @@ regexp = "[%s]{%d,}" % (chars, shortest_run)
 regexp_b = regexp.encode()
 pattern = re.compile(regexp_b)
 
-@st.cache_data
 def load_depends(app_path: Path) -> None:
     seen = set()
     with open(app_path, "rb") as f:
