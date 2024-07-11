f93fa9359e4812b6c73e400431b8e4e695655937
simocasta <127983048+simocasta@users.noreply.github.com>
2024-07-11 09:37:34 +0100

    Update classification_template_streamlit.py

--- src/autoprognosis/apps/classification/classification_template_streamlit.py
+++ src/autoprognosis/apps/classification/classification_template_streamlit.py
@@ -4,8 +4,6 @@ from typing import Any, Dict, List
 # third party
 import numpy as np
 import pandas as pd
-import plotly.express as px
-import streamlit as st
 
 # autoprognosis absolute
 import autoprognosis.logger as log
@@ -13,11 +11,12 @@ from autoprognosis.plugins.explainers import Explainers
 from autoprognosis.utils.data_encoder import EncodersCallbacks
 from autoprognosis.utils.pip import install
 
-# Import necessary packages for retry mechanism
 for retry in range(2):
     try:
+        # third party
         import plotly.express as px
         import streamlit as st
+
         break
     except ImportError:
         depends = ["streamlit", "plotly"]
@@ -53,20 +52,26 @@ def classification_dashboard(
             List of features where to plot alternative values. Example: if treatment == 0, it will plot alternative treatment == 1 as well, as a comparison.
     """
 
+    #st.set_page_config(layout="wide", page_title=title)
+
     hide_footer_style = """
         <style>
         .reportview-container .main footer {visibility: hidden;}
-        </style>
-    """
+        """
     st.markdown(hide_footer_style, unsafe_allow_html=True)
     st.markdown(
         """ <style>
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
-        </style>
-        """,
+    </style> """,
         unsafe_allow_html=True,
     )
+
+    with st.container():
+        st.markdown(
+            f"<h1 style='margin-top: -70px;'>{title}</h1>", unsafe_allow_html=True
+        )
+        st.markdown("---")
 
     CAUTION_STATEMENT = "This tool predicts your most likely outcomes based on current knowledge and data, but will never provide a 100% accurate prediction for any individual. We recommend that you discuss the results with your own specialist in a more personalised context."
 
@@ -75,7 +80,7 @@ def classification_dashboard(
     inputs = {}
     columns = []
     with menu:
-        st.markdown("<h3 style='color:#000;'>Patient info</h3>", unsafe_allow_html=True)
+        st.header("Patient info")
         
         for name, item in menu_components:
             columns.append(name)
@@ -85,23 +90,20 @@ def classification_dashboard(
                 )
                 inputs[name] = [obj]
             if item.type == "dropdown":
-                if name in ["KL grade", "Medial JSN", "Lateral JSN"]:
-                    sorted_val_range = [0, 1, 2, 3]
-                else:
-                    # Sort the val_range attribute for the dropdown options
-                    def try_float(value):
-                        try:
-                            return float(value)
-                        except ValueError:
-                            return None
+                # Sort the val_range attribute for the dropdown options
+                def try_float(value):
+                    try:
+                        return float(value)
+                    except ValueError:
+                        return None
 
-                    def is_numeric(value):
-                        return try_float(value) is not None
+                def is_numeric(value):
+                    return try_float(value) is not None
 
-                    numerical_values = sorted([x for x in item.val_range if is_numeric(str(x))], key=try_float)
-                    non_numerical_values = sorted([x for x in item.val_range if not is_numeric(str(x))])
+                numerical_values = sorted([x for x in item.val_range if is_numeric(str(x))], key=try_float)
+                non_numerical_values = sorted([x for x in item.val_range if not is_numeric(str(x))])
 
-                    sorted_val_range = numerical_values + non_numerical_values
+                sorted_val_range = numerical_values + non_numerical_values
 
                 obj = st.selectbox(
                     label=item.name,
@@ -109,33 +111,20 @@ def classification_dashboard(
                 )
                 inputs[name] = [obj]
             elif item.type == "slider_integer":
-                max_value_adjusted = ((item.max // 5) + 1) * 5
-                min_value = item.min if name != "Age" else 45
-                max_value = item.max if name != "Age" else max_value_adjusted
                 obj = st.slider(
                     item.name,
-                    min_value=min_value,
+                    min_value=item.min,
                     value=item.min,
-                    max_value=max_value,
-                    step=5
+                    max_value=item.max,
                 )
                 inputs[name] = [obj]
             elif item.type == "slider_float":
-                max_value_adjusted = ((int(item.max) // 5) + 1) * 5
-                min_value = 0.0
-                step_value = 0.1
-                if name == "WOMAC Pain Score":
-                    max_value = 20.0
-                elif name == "WOMAC Disability Score":
-                    max_value = 68.0
-                else:
-                    max_value = float(max_value_adjusted)
                 obj = st.slider(
                     item.name,
-                    min_value=min_value,
-                    value=float(item.min),
-                    max_value=max_value,
-                    step=step_value,
+                    min_value=item.min,
+                    value=item.min,
+                    max_value=item.max,
+                    step=0.1,
                 )
                 inputs[name] = [obj]
 
@@ -175,14 +164,10 @@ def classification_dashboard(
                     interpretation_df,
                     labels=dict(x="Feature", y="Source", color="Feature importance"),
                     color_continuous_scale="Blues",
-                    height=300,
+                    height=250,
                 )
-                fig.update_layout(
-                    font=dict(size=16)  # Adjust the font size as needed
-                )
-                st.markdown(
-                    f"<h3 style='color:#000;'>Feature importance for the '{reason}' risk plot using {pretty_name}</h3>", 
-                    unsafe_allow_html=True
+                st.header(
+                    f"Feature importance for the '{reason}' risk plot using {pretty_name}"
                 )
                 st.plotly_chart(fig, use_container_width=True)
 
@@ -193,58 +178,34 @@ def classification_dashboard(
 
         vals = {
             "Probability": predictions.values.squeeze(),
-            "Outcome Class": predictions.columns,
+            "Category": predictions.columns,
         }
-
-        # Convert Outcome Class to a list if it is not already
-        outcome_classes = list(vals["Outcome Class"])
-
-        if len(outcome_classes) == 2:
-            # Map the outcome classes to the desired legend labels for 2 classes
-            outcome_classes_mapped = [ 
-                {0: "0: No progression", 1: "1: Progression"}.get(int(x), x) 
-                for x in outcome_classes
-            ]
-        else:
-            # Map the outcome classes to the desired legend labels for 4 classes
-            outcome_classes_mapped = [ 
-                {0: "0: No progression", 1: "1: Pain-only progression", 2: "2: Radiographic-only progression", 3: "3: Pain and radiographic progression"}.get(int(x), x) 
-                for x in outcome_classes
-            ]
-
-        vals["Outcome Class"] = outcome_classes_mapped
-        
-        # Define custom color sequence
-        custom_color_sequence = px.colors.qualitative.Set1
-        
         fig = px.bar(
             vals,
-            x="Outcome Class",
+            x="Category",
             y="Probability",
-            color="Outcome Class",
-            color_discrete_sequence=custom_color_sequence,
-            height=450,
+            color="Category",
+            color_continuous_scale="RdBu",
+            height=300,
             width=600,
         )
         fig.update_layout(
-            xaxis_title="Outcome Class",
+            xaxis_title="Category",
             yaxis_title="Probability",
-            legend_title="Outcome Classes",
-            font=dict(size=16)  # Adjust the font size as needed
+            legend_title="Categories",
         )
 
-        st.markdown("<h3 style='color:#000;'>Predictions</h3>", unsafe_allow_html=True)
+        st.header("Predictions")
         st.plotly_chart(fig, use_container_width=True)
 
     with predictions:
-        st.markdown("<h3 style='color:#000;'>Risk estimation</h3>", unsafe_allow_html=True)
+        st.header("Risk estimation")
         st.markdown(CAUTION_STATEMENT)
 
         raw_df = pd.DataFrame.from_dict(inputs)
         df = encoders_ctx.encode(raw_df)
         
-        # Add a styled button for calculating the risk
-        
-        if st.button("Show Predictions ✋", help='Click to show predictions'):
+        # Add a button for calculating the risk
+        if st.button("Show Predictions"):
             update_predictions(raw_df, df)
             update_interpretation(df)
